dee9667e4160072cab75f6e0461a5dc0f05abd32
Zephlyn <53983014+Zephlyn@users.noreply.github.com>
2021-11-02 15:16:22 -0500

    add xml docs

--- Speaker.cs
+++ Speaker.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 
 namespace BendTime {
+	/// <summary> 
+	/// Basically just labels an audio source as a creature speaker.
+	/// </summary>
 	public class Speaker : MonoBehaviour {
-		
+		// Empty class. Just to check whether or not the audio source is a creature's speaker since it's private in BrainModuleSpeak >:(
 	}
 }
